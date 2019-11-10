f2e022e14bf4f977224e054e97cf3ea44742d54d
Nabil Kadimi <nabil@kadimi.com>
2019-11-11 00:53:44 +0100

    Do not use an anonymous class

--- class-myplugin.php
+++ class-myplugin.php
@@ -1,30 +1,8 @@
 <?php
 /**
- * Plugin Name: MyPlugin
- * Description: MyPlugin Description.
- * Text Domain: myplugin
- * Version: 1.0.0
- * Plugin URI: https://www.github.com/kadimi/starter
- * GitHub Plugin URI: https://github.com/kadimi/starter
- * Author: Nabil Kadimi
- * Author URI: https://kadimi.com
+ * The plugin class.
  *
  * @package MyPlugin
  */
 
-/**
- * Composer stuff.
- */
-require 'vendor/autoload.php';
-
-/**
- * Helper function.
- */
-function myplugin() {
-	return ( new class extends Kadimi\WPStarter {} )::get_instance();
-}
-
-/**
- * Fire plugin.
- */
-myplugin();
+class MyPlugin extends Kadimi\WPStarter {}
